bb26b3cb04a3482e98750f5d5c77435297d0c09b
AgusHeredia <agusherediatbb@gmail.com>
2023-08-01 23:54:20 -0300

    adaptamos el productManager para trabajar con FS, desafío 2 finalizado

--- src/manager/ProductManager.js
+++ src/manager/ProductManager.js
@@ -1,55 +1,154 @@
-//Creamos la clase que se encargara de manejar los productos que vayamos agregando
+//Hacemos un require al manejador de archivos nativo de NodeJs nombrado 'File System' 
+const fs = require('fs')
+
+//Creamos la clase que se encargara de manejar los productos que vayamos agregando, junto con sus archivos
 class ProductManager {
-    constructor() {
-        this.products = [];
+  constructor() {
+    this.path = "././products.txt";
+  }
+
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const products = await fs.promises.readFile(this.path, 'utf-8');
+        const productsJS = JSON.parse(products);
+        return productsJS;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.log(error);
     }
-
-    #newId(){
-        let id = 0;
-        this.products.map((product) => {
-            if (product.id >= id) id = product.id;
-        })
-        return id
-    }
+  }
 
 
-    addProduct(title, description, price, thumbnail, code, stock) {
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    try {
+      const productInfo = await this.getProducts();
+      const repeatedCode = productInfo.find((item) => item.code === code);
+
+      if (repeatedCode) {
+        console.log(`Error: El producto con el código: ${code}, ya existe!`);
+      } else {
+        const lastAddedProduct = productInfo[productInfo.length - 1];
+        const newId = lastAddedProduct ? lastAddedProduct.id + 1 : 1;
+
         const newProduct = {
-            id: this.#newId() + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
+          id: newId,
+          title,
+          description,
+          price,
+          thumbnail,
+          code,
+          stock,
+        };
 
-        }
-        this.products.push(newProduct);
+        productInfo.push(newProduct);
+        await fs.promises.writeFile(this.path, JSON.stringify(productInfo));
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    getProducts(){
-       return this.products;
+  async deleteProduct(prodId) {
+    try {
+      const productInfo = await this.getProducts();
+      const prodIndex = productInfo.find((product) => product.id === prodId);
+
+      if (prodIndex === -1) {
+        console.log('No se encontró el producto con el ID especificado');
+      } else {
+        productInfo.splice(prodIndex, 1);
+        await fs.promises.writeFile(this.path, JSON.stringify(productInfo));
+        console.log('Has eliminado el producto correctamente');
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    getProductById(idProduct){
-        return this.products.find((product) => product.id === idProduct)
+  async updateProduct(prodId, updatingProd) {
+    try {
+      const productInfo = await this.getProducts();
+      const prodIndex = productInfo.find((product) => product.id === prodId);
+
+      if (prodIndex === -1) {
+        console.log('Producto no encontrado');
+      } else {
+        const updatedProduct = {
+          ...productInfo[prodIndex],
+          ...updatingProd,
+          id: prodId,
+        };
+        productInfo[prodIndex] = updatedProduct;
+        await fs.promises.writeFile(this.path, JSON.stringify(productInfo));
+        console.log('Producto actualizado y modificado con éxito');
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
+  async #repeatedProduct(prodId) {
+    try {
+      const productInfo = await this.getProducts();
+      return productInfo.find((products) => products.id === prodId);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  async getProductById(prodId) {
+    try {
+      const product = this.#repeatedProduct(prodId);
+      if (!product) {
+        console.log('Error: Not found product');
+      } else {
+        return product;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
+//Instanciamos nuestra clase ProductManager
+const productManager = new ProductManager();
 
-// Instanciamos
-const productManager = new ProductManager()
+
+//Definimos la función asíncrona para manipular los productos
+const handleManager = async () => {
+
+  // Agregamos productos
+  await productManager.addProduct('Remera', 'Remera negra', 199, 'www.imgExample.com', '#A001', 8);
+  await productManager.addProduct('Pantalón', 'Short liso', 49, 'www.imgExample.com', '#A002', 4);
+  await productManager.addProduct('Medias', 'Medias cortas blancas', 19, 'www.imgExample.com', '#A003', 12);
+  
+  const get = await productManager.getProducts();
+  console.log("--- Llamado al array ---", get);
 
 
-//Añadimos productos a nuestro array Products[]
-productManager.addProduct('Remera', 'Remera negra', 199, 'www.imgExample.com', '#A001', 8)
-productManager.addProduct('Pantalón', 'Short liso', 49, 'www.imgExample.com', '#A002', 4)
-productManager.addProduct('Medias', 'Medias cortas blancas', 19, 'www.imgExample.com', '#A003', 12)
+  // Buscamos producto por su ID
+  const getNo2 = await productManager.getProductById(2);
+  console.log("Producto buscado por ID:", getNo2);
 
-//Imprimimos por consola nuestro array con los productos agregados
-console.log(productManager.getProducts());
 
-//Tambien podemos obtener un producto buscándolo por su ID correspondiente
-console.log('======= Buscando producto con el ID solicitado... ======', productManager.getProductById(1));
+  // OPCIONAL: Con esta función podemos eliminar un producto en específico por su ID
+  // await productManager.deleteProduct(1);
+  // const getNo4 = await productManager.getProducts();
+  // console.log("--- Tercer llamado al array ---", getNo4);
+
+
+
+  // OPCIONAL: Con esta función actualizamos las caracteristicas de un producto mediante su ID
+  // await productManager.updateProduct(1, {
+  //   price: this.price + 300,
+  //   stock: this.stock + 15,
+  // });
+
+
+
+};
+
+handleManager();
