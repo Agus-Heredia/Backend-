189f3b88686d2643bd96fa2ac7bb92433484ac2f
AgusHeredia <agusherediatbb@gmail.com>
2023-08-08 19:15:33 -0300

    instalamos express, pequeño problema con el package-lock

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-// Nuevo curso de backendd 
-console.log("Todo correctamente conectado...");
+import http from 'http'
+
+//Creamos el servidor
+const server = http.createServer((req, res) => {
+    res.end('Welcome to your new server, Agus!')
+})
+
+//Iniciamos
+const PORT = 4000
+server.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}!`);
+})
